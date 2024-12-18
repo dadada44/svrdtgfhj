a235ac5f21f0590857ae3aaca32f591fdb1e1883
daryna <07top.cz@gmail.com>
2024-12-18 12:34:58 +0100

    link

---
+++ src/lib/components/Link.svelte
@@ -0,0 +1,23 @@
+<script>
+    let {href, color, children} = $props(); // Deklarujeme atributy, které zadávame při volání komponenty
+    // $props() - je reaktivní funkce, která zajišťuje funkce atributu (tím že něco do nich napíšu, se hned projeví)
+</script>
+
+<a href="{href}" class="button {color}">
+    {@render children()} <!-- Vypisuje content mezi element -->
+</a>
+
+<style>
+    .button{
+        width: 50px;
+        background-color: red;
+    }
+
+    .ghost{
+        color: white;
+    }
+
+    .green{
+        color: green;
+    }
+</style>
---
+++ src/routes/+page.svelte
@@ -0,0 +1,9 @@
+<script>
+    import Link from "../lib/components/Link.svelte"
+</script>
+
+<h1>Welcome to SvelteKit</h1>
+<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+
+<Link href="/" color="ghost">Ahoj <!-- Zde funguje ten @render children() --></Link>
+<Link href="/" color="green">Ahoj</Link>
